a2f2115ab1ab288b16f3afbbba7ec17a69ec17cf
Ezra Miller <emiller@lsoc.org>
2015-10-14 19:42:44 -0400

    dope

--- js/GradientShader.js
+++ js/GradientShader.js
@@ -6,6 +6,8 @@ function GradientShader(){
 				"mouse"  : { type: "v2", value: null },
 				"resolution"  : { type: "v2", value: null },
 				"time"  : { type: "f", value: null },
+				"bgO"  : { type: "f", value: 0.0 },
+				"texO"  : { type: "f", value: 1.0 },
 
 			}
 		]);
@@ -27,28 +29,35 @@ function GradientShader(){
 			"uniform vec2 resolution;",
 			"uniform vec2 mouse;",
 			"uniform float time;",
+			"uniform float bgO;",
+			"uniform float texO;",
 			"varying vec2 vUv;",
 
 			"void main(){",
 			"	vec2 uv = vUv;",
+			"	vec4 color = texture2D(texture, uv);",
 			"	if(mod(gl_FragCoord.x, 0.03) == 0.0){",
 			"		if(mod(gl_FragCoord.y, 0.03) == 0.0){",
-			"			gl_FragColor = vec4(uv,0.5+0.5*sin(time*2.0),1.0);",
-			// "			gl_FragColor = texture2D(texture, uv);",
+			// "			gl_FragColor = vec4(uv,0.5+0.5*sin(time*2.0),1.0);",
+			"			gl_FragColor = vec4(color.rgb, texO);",
 			// "			gl_FragColor = vec4(1.0,1.0,1.0,1.0);",
+			// "			gl_FragColor = texture2D(texture, uv);",
+
 			// "		}",
 			// "	}",
 			"		} else {",
-			"			gl_FragColor = texture2D(texture, uv);",
-			// "			gl_FragColor = vec4(1.0,1.0,1.0,1.0);",
+			// "			gl_FragColor = texture2D(texture, uv);",
+			// "			gl_FragColor = vec4(1.0,1.0,1.0,bgO);",
 
-			// "			gl_FragColor = vec4(uv,0.5+0.5*sin(time*2.0),1.0);",
+			"			gl_FragColor = vec4(uv,0.5+0.5*sin(time*2.0),bgO);",
 			"		}",
 			"	} else {",
-			"			gl_FragColor = texture2D(texture, uv);",
+			// "			gl_FragColor = texture2D(texture, uv);",
+			// "			gl_FragColor = vec4(1.0,1.0,1.0,bgO);",
+
 			// "			gl_FragColor = texture2D(texture2, uv);",
 // 
-			// "		gl_FragColor = vec4(uv,0.5+0.5*sin(time*2.0),1.0);",
+			"		gl_FragColor = vec4(uv,0.5+0.5*sin(time*2.0),bgO);",
 			"	}",
 			"}"
 
